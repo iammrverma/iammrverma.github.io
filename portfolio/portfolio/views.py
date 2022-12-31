ad123cbe9145938a6d40b7c4da0a7ae36d2ce423
RAJ <rajwebz2020@gmail.com>
2023-01-01 01:12:21 +0530

    introducing navbar and associated files
    adding templates in DIRS list in settings.py
    adding two urls - view.index, view.contact in urls.py
    
    adding file portfolio/portfolio/views.py
    	index function -> render index.html
    	contact function -> render contact.html
    
    adding files in portfolio/templates
    	index.html -> the home page
    	contact.html

---
+++ portfolio/portfolio/views.py
@@ -0,0 +1,11 @@
+""" Owner Raj Verma """
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
